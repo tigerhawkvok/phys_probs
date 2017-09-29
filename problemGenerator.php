9486823d40a8948a447fcebb7a45ff4e3c979952
Philip Kahn <tigerhawkvok@gmail.com>
2017-09-29 08:11:20 -0700

    Add CDN stuff

--- problemGenerator.php
+++ problemGenerator.php
@@ -91,7 +91,13 @@ if (isset($_REQUEST["generator_problem"])) {
 <!doctype html>
 <html>
   <head>
-    
+    <title>
+      Physics Problem Generator
+    </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="js/c.min.js" type="text/javascript"></script>
   </head>
   <body class="container-fluid">
     
